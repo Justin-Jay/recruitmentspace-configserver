427186d42f2815d232bcf36c8a61090ab2fb64e9
Justin-Jay <56620907+Justin-Jay@users.noreply.github.com>
2023-03-05 19:34:38 +0200

    Add files via upload

---
+++ src/main/java/com/recruitmentspaceconfigserver/recruitmentspaceconfigserver/RecruitmentspaceConfigserverApplication.java
@@ -0,0 +1,13 @@
+package com.recruitmentspaceconfigserver.recruitmentspaceconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecruitmentspaceConfigserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecruitmentspaceConfigserverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/recruitmentspaceconfigserver/recruitmentspaceconfigserver/RecruitmentspaceConfigserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.recruitmentspaceconfigserver.recruitmentspaceconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecruitmentspaceConfigserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
